e947959eeb505ca5d9fd52e5842d1158445f7ca8
mirackarakasli <ilhansamikara@gmail.com>
2021-11-04 21:41:08 +0300

    2.ders sonu

--- weather_app/views.py
+++ weather_app/views.py
@@ -1,12 +1,38 @@
 from django.shortcuts import render
 import requests
 from decouple import config 
+from pprint import pprint
+from .models import City
 
 def index(request):
-    url = "https://api.openweathermap.org/data/2.5/weather?q={}&appid={}"
-    city = "istanbul"
-    response = requests.get(url.format(city, config('API_KEY')))
-    return render(request, 'weather_app/index.html')
+    cities = City.objects.all()
+    url = "https://api.openweathermap.org/data/2.5/weather?q={}&appid={}&units=metric"
+    # city = "istanbul"
+    # response = requests.get(url.format(city, config('API_KEY')))
+    # content = response.json()
+    # pprint(content)
+    # print(type(content))
+    city_data = []
+    for city in cities:
+        # print(city)
+        response = requests.get(url.format(city, config('API_KEY')))
+        content = response.json()
+        # pprint(content)
+        # print(type(content))
+        data = {
+            'city': city,
+            'temp': content['main']['temp'],
+            'desc': content['weather'][0]['description'],
+            'icon': content['weather'][0]['icon']            
+        }
+        city_data.append(data)
+        # print(city_data)
+
+        context = {
+            'city_data': city_data,
+        }
+
+    return render(request, 'weather_app/index.html', context)
 
 
 
