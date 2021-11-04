0d06ee3b0daa5f1817ad9eb799c296ed452232c9
mirackarakasli <ilhansamikara@gmail.com>
2021-11-04 20:47:09 +0300

    1.ders

--- weather_app/views.py
+++ weather_app/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+import requests
+from decouple import config 
 
 def index(request):
+    url = "https://api.openweathermap.org/data/2.5/weather?q={}&appid={}"
+    city = "istanbul"
+    response = requests.get(url.format(city, config('API_KEY')))
     return render(request, 'weather_app/index.html')
+
+
+
